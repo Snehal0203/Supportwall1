a03ebe8c568f186a2a551b0a80456cd741082740
Snehal Adbol <smitaadbol01@gmail.com>
2021-09-24 20:42:21 +0530

    Update settings.py

--- supportwall/supportwall/settings.py
+++ supportwall/supportwall/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c%j4rh(yt%x!jlt=r)vh-_i)+5mzo)*=s^okc#=-xan3j73h)#'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
